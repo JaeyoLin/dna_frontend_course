50daecbf589cb802a55b7c071707a89183b4f3a3
Aaron Lin <aaron_lin@wistron.com>
2019-07-29 16:51:05 +0800

    Add vue

---
+++ course/chapter4/vue-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Hello backgroundColor="green" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ course/chapter4/vue-app/src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="helloCSS" v-bind:style="style">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    backgroundColor: String
+  },
+  computed: {
+    style () {
+      return 'background-color: ' + this.backgroundColor;
+    }
+  },
+}
+</script>
+
+<style scoped>
+.helloCSS{
+  width: 200px;
+  height: 50px; 
+  line-height: 50px;
+  font-size: 50px;
+  border-radius: 10px;
+  color: black;
+}
+</style>
