26ff0168b67b08c06778186bb0168edb00ec3d77
Aaron Lin <aaron_lin@wistron.com>
2019-07-29 17:08:50 +0800

    Revise vue

--- course/chapter4/vue-app/src/App.vue
+++ course/chapter4/vue-app/src/App.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import Hello from './components/Hello.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    Hello
+    // HelloWorld,
+    Hello,
   }
 }
 </script>
